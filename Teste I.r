fec9a169e6dbe2e3e97301440b28f64fdf60e48f
FelipeTestadeSouza <felipetesta@alunos.utfpr.edu.br>
2024-02-08 10:49:24 -0300

    commit initial

---
+++ Teste I.r
@@ -0,0 +1,9 @@
+usethis::create_project("D:/Google/UTFPR/IC/teste") #Código - criar projeto
+
+usethis::use_git_config(user.name = "FelipeTestadeSouza", user.email = "felipetesta@alunos.utfpr.edu.br")
+
+usethis::create_github_token()
+
+usethis::edit_r_environ()
+
+usethis::git_sitrep()
